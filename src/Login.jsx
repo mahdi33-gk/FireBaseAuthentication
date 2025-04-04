a5426c682cc9dbf4bf996a5c6f794259b6ebee1c
mehedi <mehedi25122003@gmail.com>
2025-04-04 16:38:40 +0600

    github commit method added

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,9 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from "./firebase.init";
 import { useState } from "react";
 
 const provider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 const Login = () => {
 
@@ -25,13 +26,28 @@ const Login = () => {
         .catch(()=>{
             console.log('found err')
         })
+        
+    }
+    const handlegithub = () => {
+        signInWithPopup(auth,githubProvider)
+        .then((result)=>{
+            setuser(result.user)
+        })
+        .catch(()=>{
+            setuser(null)
+        })
     }
     const [user, setuser] = useState(null);
     return (
         <div>
             <h3>Login page</h3>
             
-            {user? <button onClick={handlesignOut}>Sign out</button>:<button onClick={clickHandler}>Login with google</button>}
+            {user? <button onClick={handlesignOut}>Sign out</button>:
+            <>
+            <button onClick={clickHandler}>Login with google</button>
+            <button onClick={handlegithub}>Login with github</button>
+            </>
+            }
             {
                 user && <div>
                     <h3>{user.displayName
