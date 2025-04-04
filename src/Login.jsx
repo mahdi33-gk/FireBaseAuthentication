e6fe86d315783b4b819ef152ece9696ee66ad533
mehedi <mehedi25122003@gmail.com>
2025-04-04 16:07:23 +0600

    firebase image collecting issue solved

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,8 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from "./firebase.init";
+import { useState } from "react";
+
+const provider = new GoogleAuthProvider();
 
 const Login = () => {
 
@@ -7,17 +10,37 @@ const Login = () => {
         signInWithPopup(auth, provider)
         .then((result)=>{
             console.log(result)
+            setuser(result.user)
         })
         .catch(err => {
             console.log(err)
+            setuser(null)
         })
     }
-    const provider = new GoogleAuthProvider();
-   
+    const handlesignOut = () =>{
+        signOut(auth)
+        .then(()=>{
+            console.log('signout done')
+        })
+        .catch(()=>{
+            console.log('found err')
+        })
+    }
+    const [user, setuser] = useState(null);
     return (
         <div>
             <h3>Login page</h3>
             <button onClick={clickHandler}>Login with google</button>
+            <button onClick={handlesignOut}>Sign out</button>
+            {
+                user && <div>
+                    <h3>{user.displayName
+                }</h3>
+                <h3>Email: {user.email}</h3>
+                <img  src={user.photoURL}  alt="User Profile" width="100" height="100" />
+                </div>
+                
+            }
         </div>
     );
 };
